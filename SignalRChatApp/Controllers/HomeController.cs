3b084ba334680d79eeef61ff981aa36ba53d1609
Mario <mvilla2208@gmail.com>
2014-11-10 16:01:51 -0600

    page fix

--- SignalRChatApp/Controllers/HomeController.cs
+++ SignalRChatApp/Controllers/HomeController.cs
@@ -5,30 +5,12 @@ namespace SignalRChatApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            return View();
+        public HomeController()
+        {
+            Chat();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        /// <summary>
-        /// for return Chat View
-        /// </summary>
         public ActionResult Chat()
         {
             return View();
